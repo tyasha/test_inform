2fb1b4308859939d3f5f88909dc04d650f4d4f4c
Yakov Byakov <yakovfox@mail.ru>
2025-05-05 12:17:54 +0300

    Решение тестовой задачи

--- src/main/java/org/yakov/Main.java
+++ src/main/java/org/yakov/Main.java
@@ -1,9 +1,18 @@
 package org.yakov;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(countNumbers(new int[]{1, 3, 4, 5, 1, 5, 4}));
     }
 
-    public static
+    public static Map<Integer, Long> countNumbers(int[] numbers) {
+        return IntStream.of(numbers)
+                .boxed()
+                .collect(Collectors.groupingBy(num -> num,Collectors.counting()));
+    }
 }
